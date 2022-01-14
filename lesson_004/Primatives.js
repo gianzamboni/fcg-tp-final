1fb06bc254a5153f669cb151549a5b313d922118
Fede Sassone <fssassone@gmail.com>
2022-01-14 10:20:17 -0300

    dibujar laberinto WIP

--- lesson_004/Primatives.js
+++ lesson_004/Primatives.js
@@ -1,50 +1,161 @@
 var Primatives = {};
 Primatives.GridAxis = class {
-	static createMesh(gl, x, y){
+	static createMesh(gl, laberinto){
 		//Dynamiclly create a grid
+		var x = laberinto.x;
+		var y = laberinto.y;
+		var matriz = laberinto.matriz;
 		var verts = [],
 			size = 1.8;			// W/H of the outer box of the grid, from origin we can only go 1 unit in each direction, so from left to right is 2 units max
 			// div = 10.0,			// How to divide up the grid
 			// step = size / div,	// Steps between each line, just a number we increment by for each line in the grid.
 			// half = size / 2;	// From origin the starting position is half the size.
 
-		var p;	//Temp variable for position value.
-		// lineas horizontales
-		var div = x;
-		var step = size/ x;
-		var half = size / 2;
-		for(var i=0; i <= div; i++){
+		var px1;	//Temp variable for position value.
+		var px2;
+		var px;
+		var py;
+		var py1;
+		var py2;
+		var stepX = size/ x;
+		var half = size / 2
+		var stepY = size/ y;
 
-			//Horizontal line
-			p = half - (i * step);
-			verts.push(-half);	//x1
-			verts.push(p);		//y1
-			verts.push(0);		//z1
-			verts.push(0);		//c1
+		// var tx = size / x;
+		// const tx = (px) => ((px/x) * stepX )-0.9;  
+		const tx = (px) => (px * stepX )-0.9;
+		const ty = (py) => (py * stepY )-0.9;
+		// var ty = size / y;
+		// test dibujar de a celdas 
+		var celda, celdaArriba, celdaAbajo, celdaIzq, celdaDer;
+		// console.log(laberinto);
+		for(var i=1; i<x-1; i++){
+			
+			for(var j=1; j<y-1; j++){
+				celda = matriz[i][j];
+				celdaArriba = matriz[i][j-1];
+				celdaAbajo = matriz[i][j+1];
+				celdaIzq = matriz[i-1][j];
+				celdaDer = matriz[i+1][j];
 
-			verts.push(half);	//x2
-			verts.push(p);		//y2
-			verts.push(0);		//z2
-			verts.push(1);		//c2
+				// caso tenemos que dibujar "paredes"
+				if(celda.estado==="abierta"){
+					// dibujamos las de arriba
+
+					
+					if(celdaArriba.estado==="cerrada"){
+						console.log(celda);
+						console.log(celdaArriba);
+						// px1 = half - (tx * celdaArriba.x);
+						px1 = tx(celdaArriba.x);
+						px2 = tx(celdaArriba.x + 1);
+						// px2 = half - (tx * (celdaArriba.x +1));
+						// px = half - (i * stepX);
+						// py = half - (j * stepY);
+						// py = half - (ty * celdaArriba.y);
+						py = ty(celdaArriba.y);
+						// py2 = ty * (celda.y+1);
+						verts.push(px1);	//x1
+						verts.push(py);		//y1
+						verts.push(0);		//z1
+						verts.push(0);		//c1
+ 
+						verts.push(px2);	//x2
+						verts.push(py);		//y2
+						verts.push(0);		//z2
+						verts.push(0);		//c2
+					}
+					// // las de abajo
+					// if(celdaAbajo.estado==="cerrada"){
+					// 	px1 = half - (tx * celdaAbajo.x);
+					// 	px2 = half - (tx * (celdaAbajo.x +1));
+					// 	// px = half - (i * stepX);
+					// 	// py = half - (j * stepY);
+					// 	py = half - (ty * celdaAbajo.y);
+					// 	// py2 = ty * (celda.y+1);
+					// 	verts.push(px1);	//x1
+					// 	verts.push(py);		//y1
+					// 	verts.push(0);		//z1
+					// 	verts.push(0);		//c1
+ 
+					// 	verts.push(px2);	//x2
+					// 	verts.push(py);		//y2
+					// 	verts.push(0);		//z2
+					// 	verts.push(0);		//c2
+					// }
+					// // las de izq
+					// if(celdaIzq.estado==="cerrada"){
+					// 	px = half - (tx * celdaIzq.x);
+					// 	// px2 = half - (tx * (celdaAbajo.x +1));
+					// 	// px = half - (i * stepX);
+					// 	// py = half - (j * stepY);
+					// 	py1 = half - (ty * celdaIzq.y);
+					// 	py2 = half - (ty * celdaIzq.y +1);
+					// 	// py2 = ty * (celda.y+1);
+					// 	verts.push(px);	//x1
+					// 	verts.push(py1);		//y1
+					// 	verts.push(0);		//z1
+					// 	verts.push(0);		//c1
+ 
+					// 	verts.push(px);	//x2
+					// 	verts.push(py2);		//y2
+					// 	verts.push(0);		//z2
+					// 	verts.push(0);		//c2
+					// }
+				}
+			}
+			
 		}
+		// verts.push(0);
+		// verts.push(0);
+		// verts.push(0);
+		// verts.push(0);
+
+		// verts.push(size/x);
+		// verts.push(0);
+		// verts.push(0);
+		// verts.push(0);
+
+
+		// lineas horizontales
+		// funcando
+		// var div = x;
+		// var step = size/ x;
+		// var half = size / 2;
+		// for(var i=0; i <= div; i++){
+
+
+
+		// 	//Horizontal line
+		// 	p = half - (i * step);
+		// 	verts.push(-half);	//x1
+		// 	verts.push(p);		//y1
+		// 	verts.push(0);		//z1
+		// 	verts.push(0);		//c1
+
+		// 	verts.push(half);	//x2
+		// 	verts.push(p);		//y2
+		// 	verts.push(0);		//z2
+		// 	verts.push(1);		//c2
+		// }
 
 		// lineas verticales
-		div = y;
-		step = size/ y;
-		half = size / 2;
-		for(var i=0; i <= div; i++){
-			//Vertical line
-			p = -half + (i * step);
-			verts.push(p);		//x1
-			verts.push(half);	//y1
-			verts.push(0);		//z1
-			verts.push(0);		//c2
+		// div = y;
+		// step = size/ y;
+		// half = size / 2;
+		// for(var i=0; i <= div; i++){
+		// 	//Vertical line
+		// 	p = -half + (i * step);
+		// 	verts.push(p);		//x1
+		// 	verts.push(half);	//y1
+		// 	verts.push(0);		//z1
+		// 	verts.push(0);		//c2
 
-			verts.push(p);		//x2
-			verts.push(-half);	//y2
-			verts.push(0);		//z2
-			verts.push(1);		//c2
-		}
+		// 	verts.push(p);		//x2
+		// 	verts.push(-half);	//y2
+		// 	verts.push(0);		//z2
+		// 	verts.push(1);		//c2
+		// }
 
 		//TODO : Remove the following, its only to demo extra lines can be thrown in.
 		// verts.push(-half);	//x1
