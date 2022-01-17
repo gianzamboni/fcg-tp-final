f5985ca08d30cf569bd2aa1a5ea28d95771da152
Federico Sassone <fssassone@gmail.com>
2022-01-17 12:36:43 -0300

    agrega transformaciones que no hacen nada

--- lesson_004/Modal.js
+++ lesson_004/Modal.js
@@ -1,11 +1,20 @@
 class Modal{
 	constructor(meshData){
-		//TODO :: create this.transform in future lesson
+		this.transform = new Transform();
 		this.mesh = meshData;
 	}
 
+	//--------------------------------------------------------------------------
+	//Getters/Setters
+	setScale(x,y,z){ this.transform.scale.set(x,y,z); return this; }
+	setPosition(x,y,z){ this.transform.position.set(x,y,z); return this; }
+	setRotation(x,y,z){ this.transform.rotation.set(x,y,z); return this; }
+
+	addScale(x,y,z){	this.transform.scale.x += x;	this.transform.scale.y += y;	this.transform.scale.y += y;	return this; }
+	addPosition(x,y,z){	this.transform.position.x += x; this.transform.position.y += y; this.transform.position.z += z; return this; }
+	addRotation(x,y,z){	this.transform.rotation.x += x; this.transform.rotation.y += y; this.transform.rotation.z += z; return this; }
+
+	//--------------------------------------------------------------------------
 	//Things to do before its time to render
-	preRender(){ 
-		//TODO :: Update transform Matrix in future lesson
-	}
+	preRender(){ this.transform.updateMatrix(); return this; }
 }
